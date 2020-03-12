8ea75b2183d88919b006354e4d3b09dc61c6b1c3
Abda Shaffan <abdashaffan@gmail.com>
2020-03-12 10:19:29 +0700

    Debug fragment

--- app/src/main/java/com/example/bolaksepak/teaminfo/TeamInfoActivity.java
+++ app/src/main/java/com/example/bolaksepak/teaminfo/TeamInfoActivity.java
@@ -1,6 +1,7 @@
 package com.example.bolaksepak.teaminfo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -25,7 +26,6 @@ public class TeamInfoActivity extends AppCompatActivity {
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                closeFragment();
                 Log.d("EVENT", "ONTABSELECTED");
                 int pos = tab.getPosition();
                 switch (pos) {
@@ -45,7 +45,6 @@ public class TeamInfoActivity extends AppCompatActivity {
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
                 Log.d("EVENT", "ONTABUNSELECTED");
-                closeFragment();
             }
 
             @Override
@@ -62,6 +61,11 @@ public class TeamInfoActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        NavUtils.navigateUpFromSameTask(this);
+    }
+
     public void displayFragment() {
         Log.d("EVENT", "DISPLAYFRAG");
         int[] clubImages;
@@ -73,22 +77,22 @@ public class TeamInfoActivity extends AppCompatActivity {
             clubImages = new int[]{R.drawable.club3, R.drawable.club3};
         }
         MatchListFragment fragment = MatchListFragment.newInstance(clubImages);
+
         FragmentManager fragmentManager = getSupportFragmentManager();
-
-
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.match_fragment_container, fragment).addToBackStack(null).commit();
+
+        if (fragmentTransaction != null) {
+            fragmentTransaction.replace(R.id.match_fragment_container, fragment).addToBackStack(null).commit();
+        }
     }
 
     public void closeFragment() {
         Log.d("EVENT", "DESTROYFRAG");
         // Get the FragmentManager.
         FragmentManager fragmentManager = getSupportFragmentManager();
-        // Check to see if the fragment is already showing.
         MatchListFragment simpleFragment = (MatchListFragment) fragmentManager
                 .findFragmentById(R.id.match_fragment_container);
         if (simpleFragment != null) {
-            // Create and commit the transaction to remove the fragment.
             FragmentTransaction fragmentTransaction =
                     fragmentManager.beginTransaction();
             fragmentTransaction.remove(simpleFragment).commit();
